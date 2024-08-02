6d60e1f4969e3a8eacb8e550affc7fc2b5b7310f
kasai-jeyes <kasai_s@jeyes.co.jp>
2024-08-02 15:43:58 +0900

    名前を入力してください。のラベルを追加した。 入力欄のテキストフィールドを追加した。 次の画面へ遷移するためのボタンを追加した。 次の画面（挨拶する画面）を追加した。 挨拶する画面にこんにちは名前さんのラベルを追加した。 前の画面に戻るボタンを追加した。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+ 
+    @IBOutlet weak var textfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        //segue から遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.str = textfield.text
+        
+    }
+        
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
     }
 
 
